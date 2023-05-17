bd91cc9d4ba858c2764d41b87c29a89af5115a41
Erin <rena4242@hotmail.com>
2023-05-17 08:55:17 +0200

    Update scoreboard.java

--- SportRader/src/main/java/footballscoreboard/service/scoreboard.java
+++ SportRader/src/main/java/footballscoreboard/service/scoreboard.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class scoreboard {
+public class ScoreBoard {
 
     private Team homeTeam,awayTeam;
     List<List<Team>> matchAdded;
